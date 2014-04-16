e84e3e4f14314ca89b854298a349c7d35a009e05
Alexandra <sakujo8744@mail.ru>
2014-04-16 17:42:01 +0400

    что-то печальное js

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ function getAll() { //функция,в которой мы берем все э
 window.onload = function () { //то,что выполняется после загрузки страницы
   getAll()
 }
-
-
-
+if (nameinput==null) {
+  nameinput.borderColor = rgb(255,0,0)
+}
+else { nameinput.borderColor =  rgb(0,255,0)
 
