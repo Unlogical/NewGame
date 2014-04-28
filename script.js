bf7a085fd9bfd2de4527f96fe7551c2e08514920
Alexandra <sakujo8744@mail.ru>
2014-04-28 19:21:45 +0400

    js validate login

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function validateInput(input, reg) { 
   return function() {
     var testIt = reg.test(input.value) 
-    input.style.borderColor = testIt  ? 'green' : 'red'
-    if(!testIt) {return false}
+    input.style.backgroundImage = testIt  ? 'url(style/checkmark.png)' : 'url(style/close1.ico)'
   }
 }
-function validatePassword() {
+function validatePasswords() {
   var pass1 = document.forms["registration"].pass
   var pass2 = document.forms["registration"].copypass 
   if (!pass1.value  || pass1.value !=pass2.value ) {
   	 pass1.style.borderColor = 'red'
-  	 pass2.style.borderColor = 'red'
+  	 pass2.style.backgroundImage = 'red'
     return false
   } 
   return true 
@@ -19,10 +18,13 @@ function validatePassword() {
 window.onload = function () {
   var form = document.forms["registration"]
   var emailregexp = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]*\.[a-z]{2,6}$/ 
+  var loginregexp = /^[a-z0-9_-]{4,16}$/
   var validateEmail = validateInput(form.email,emailregexp)
+  var validateLogin = validateInput(form.login,loginregexp)
   form.email.onkeyup = validateEmail
+  form.login.onkeyup = validateLogin
   document.getElementById("regbutton").onclick = function () {
-    if (validatePassword()) {
+    if (validatePasswords()) {
       form.submit()    
     }
     return false    
