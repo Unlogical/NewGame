34f75581974c258370af9407d5eab1495e822949
Alexandra <sakujo8744@mail.ru>
2014-05-06 20:49:27 +0400

    carousel format

--- js/carousel.js
+++ js/carousel.js
@@ -1,43 +1,44 @@
-  (function($) {
-     $(function() {
-       $('.jcarousel').jcarousel({
-        wrap: 'circular'
-         })
-
-        $('.jcarousel-control-prev')
-          .on('jcarouselcontrol:active', function() {
-            $(this).removeClass('inactive')
-          })
-          .on('jcarouselcontrol:inactive', function() {
-            $(this).addClass('inactive')
-          })
-          .jcarouselControl({
-            target: '-=1'
-          })
-
-        $('.jcarousel-control-next')
-          .on('jcarouselcontrol:active', function() {
-            $(this).removeClass('inactive')
-          })
-          .on('jcarouselcontrol:inactive', function() {
-            $(this).addClass('inactive')
-          })
-          .jcarouselControl({
-            target: '+=1'
-          })
-
-        $('.jcarousel-pagination')
-          .on('jcarouselpagination:active', 'a', function() {
-            $(this).addClass('active')
-          })
-          .on('jcarouselpagination:inactive', 'a', function() {
-            $(this).removeClass('active')
-          })
-       .jcarouselPagination()
-       $('.jcarousel').jcarouselAutoscroll({
-            interval: 6000,
-            target: '+=1',
-            autostart: true
-        })
+(function($) {
+  $(function() {
+    $('.jcarousel').jcarousel({
+      wrap: 'circular'
     })
-  })(jQuery)
+    
+    $('.jcarousel-control-prev')
+      .on('jcarouselcontrol:active', function() {
+        $(this).removeClass('inactive')
+      })
+      .on('jcarouselcontrol:inactive', function() {
+        $(this).addClass('inactive')
+      })
+      .jcarouselControl({
+        target: '-=1'
+      })
+      
+    $('.jcarousel-control-next')
+      .on('jcarouselcontrol:active', function() {
+        $(this).removeClass('inactive')
+      })
+      .on('jcarouselcontrol:inactive', function() {
+        $(this).addClass('inactive')
+      })
+      .jcarouselControl({
+        target: '+=1'
+      })
+
+    $('.jcarousel-pagination')
+      .on('jcarouselpagination:active', 'a', function() {
+        $(this).addClass('active')
+      })
+      .on('jcarouselpagination:inactive', 'a', function() {
+        $(this).removeClass('active')
+      })
+    .jcarouselPagination()
+    
+    $('.jcarousel').jcarouselAutoscroll({
+      interval: 6000,
+      target: '+=1',
+      autostart: true
+    })
+  })
+})(jQuery)
